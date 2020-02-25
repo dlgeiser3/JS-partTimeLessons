3860d5b95e0e9789cfc288eaf238274d3f454c0d
Danielle <dlgeiser2992@gmail.com>
2020-02-25 16:42:07 -0500

    wk2 day2 lightning rounds

--- Week2/lightningRounds.js
+++ Week2/lightningRounds.js
@@ -67,3 +67,145 @@ switch (true) {
 // ANSWER
 let num = 0;
 num % 2 === 0 ? console.log('even') : console.log('odd')
+
+
+
+
+
+// ************************************************************
+//   *************** DAY 2 LIGHTNING ROUNDS ***************
+// ************************************************************
+
+
+
+// QUESTION
+// Write a function that accepts two numbers as parameters 
+// console.log the sum of the two numbers
+
+
+
+
+//ANSWER
+function add(a, b) {
+  console.log(a + b)
+}
+
+add(3, 6)
+
+
+function add(a, b) {
+  let sum = (a + b)
+  console.log(sum)
+}
+
+add(3, 6)
+
+
+// QUESTION
+// Write a for loop that counts to 20 by 2
+
+
+
+
+// ANSWER
+for(i = 0; i <= 20; i += 2){
+  console.log(i)
+}
+
+
+
+
+// QUESTION
+// Write a function that accepts a name parameter and console.logs 'Hi name!'
+// Invoke that function
+
+
+
+
+// ANSWER
+function sayHi(name) {
+  console.log(`Hi ${name}!`)
+}
+
+sayHi('Finn')
+
+
+
+// QUESTION
+// Write a function that counts to 10 and console.logs each number
+
+
+
+
+
+// ANSWER
+function count() {
+  for(i = 0; i <= 10; i++ ){
+    console.log(i)
+  }
+}
+
+count()
+
+
+
+// QUESTION
+// Write an fat arrow function
+
+
+
+
+
+//ANSWER
+let mario = ( ) => { console.log("It's a me, Mario!") }
+
+mario( );
+
+
+
+// QUESTION
+// Write a function that accepts a parameter of movie. Using string interpolation console log your favorite movie.
+
+
+
+
+// ANSWER
+function favMovie(movie) {
+  console.log(`My favorite movie is ${movie}`)
+}
+
+favMovie('Spirited Away')
+
+
+
+// QUESTION
+// Using the following array, write a for loop that logs each item in the array.
+let characters = ['Link', 'Zelda', 'Gannon', 'Navi']
+
+
+
+
+
+
+// ANSWER
+let characters = ['Mario', 'Luigi', 'Peach', 'Bowser']
+
+for (char of characters) {
+  console.log(char)
+}
+
+
+
+// QUESTION
+//Use a for in loop to console.log each of the keys in the following object
+
+let character = { 
+  name:"Link", 
+  isZelda: false, 
+  weapon: "Sword", 
+  hearts: 10 
+  };
+
+  for(item in character){
+    console.log(`${item}`)
+  }
